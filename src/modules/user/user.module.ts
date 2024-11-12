d5f08ea60538b21322699e6c6b38b518717b51fc
dnchuong17 <chuongdo172@gmail.com>
2024-11-12 09:58:20 +0700

    fix

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -7,7 +7,13 @@ import { UserSettingInfo } from './entity/user-info.entity';
 import { ScheduleTemplateModule } from '../schedulerTemplate/scheduleTemplate.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, UserSettingInfo, ScheduleTemplateModule])],
+  imports: [
+    TypeOrmModule.forFeature([
+      UserEntity,
+      UserSettingInfo,
+      ScheduleTemplateModule,
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
