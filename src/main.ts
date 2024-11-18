47a736834efd5ffafcf32c06667d21d95d7ee1a8
dnchuong17 <chuongdo172@gmail.com>
2024-11-19 01:37:03 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { TracingLoggerMiddleware } from './logger/tracing-logger.middleware';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
   app.use(helmet());
   app.use(cookieParser());
@@ -15,7 +15,10 @@ async function bootstrap() {
   const logger = new Logger();
   logger.log('Server is running in http://localhost:3000.');
   app.enableCors({
-    origin: 'iuscheduler-production.up.railway.app',
+    origin: [
+      'https://alert-server-production-937d.up.railway.app',
+      'http://localhost:5173/',
+    ],
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
