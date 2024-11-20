9f8136ae75e6fb3992d7c4bc1faa0e79ce72cea5
dnchuong17 <chuongdo172@gmail.com>
2024-11-20 23:56:55 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,6 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
-  app.use(helmet());
-  app.use(cookieParser());
-  app.use(TracingLoggerMiddleware);
-  app.setGlobalPrefix('/api');
-  const logger = new Logger();
-  logger.log('Server is running in http://localhost:3000.');
   app.enableCors({
     origin: [
       'https://alert-server-production-937d.up.railway.app',
@@ -22,5 +16,11 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type, Authorization'],
     credentials: true,
   });
+  app.use(helmet());
+  app.use(cookieParser());
+  app.use(TracingLoggerMiddleware);
+  app.setGlobalPrefix('/api');
+  const logger = new Logger();
+  logger.log('Server is running in http://localhost:3000.');
 }
 bootstrap();
