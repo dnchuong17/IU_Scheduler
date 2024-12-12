c57f59a9939fc59ad275f0f354f584372ff2aa44
dnchuong17 <chuongdo172@gmail.com>
2024-12-12 21:35:29 +0700

    update signin

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import * as cookieParser from 'cookie-parser';
 import { TracingLoggerMiddleware } from './logger/tracing-logger.middleware';
 import { Logger } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(helmet());
   app.use(cookieParser());
   app.setGlobalPrefix('/api');
+  app.use(compression());
   app.enableCors({
     origin: 'http://localhost:5173', // Hoặc '*' để cho phép tất cả
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
