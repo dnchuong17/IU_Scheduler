ada504054ce0de22a9b97680685b7a45c1657880
Trần Quốc Anh <135408372+TrQAnhh@users.noreply.github.com>
2024-12-20 18:33:37 +0700

    300th update

--- src/modules/deadline/deadline.module.ts
+++ src/modules/deadline/deadline.module.ts
@@ -5,10 +5,11 @@ import { DeadlineService } from './service/deadline.service';
 import { DeadlineController } from './controller/deadline.controller';
 import { CourseValueService } from '../courseValue/service/courseValue.service';
 import { CourseValueEntity } from '../courseValue/entity/courseValue.entity';
+import { TracingLoggerService } from "../../logger/tracing-logger.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([DeadlineEntity, CourseValueEntity])],
   controllers: [DeadlineController],
-  providers: [DeadlineService, CourseValueService],
+  providers: [DeadlineService, CourseValueService, TracingLoggerService],
 })
 export class DeadlineModule {}
